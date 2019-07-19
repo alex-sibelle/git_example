7b0e521f42c876585f359fb21e70f10ea9c9bfcb
Alex Sibelle <alexsibelle94@gmail.com>
2019-07-19 06:40:02 +0000

    calculated some means, by adding group by and summarise

--- myGREATESTproject.R
+++ myGREATESTproject.R
@@ -4,6 +4,9 @@ mtcars$car <- rownames(mtcars)
 
 store <-   
 mtcars %>% 
-  filter(mpg < 20 & cyl <= 6)
+  filter(mpg < 20 & cyl <= 6) %>%
+  group_by(cyl) %>% 
+  summarise_at(c("vs","mpg","disp","hp"), mean)
 
-store
+
+
